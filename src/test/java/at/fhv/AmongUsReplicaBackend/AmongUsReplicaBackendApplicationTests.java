9f2b508c16de51e0026d70825ee73794d9bc4adc
Lea Roncero PC <lea.roncero@students.fhv.at>
2024-03-20 20:37:24 +0100

    first commit - testing websockets

---
+++ src/test/java/at/fhv/AmongUsReplicaBackend/AmongUsReplicaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhv.AmongUsReplicaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmongUsReplicaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
