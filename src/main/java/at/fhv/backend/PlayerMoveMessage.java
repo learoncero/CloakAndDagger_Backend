dd832427c59c84e926221fe0ee85a30dcdcdcd77
Sarah Neville <sarah.neville@students.fhv.at>
2024-03-25 15:06:58 +0100

    updated MoveMessage to match the message received via ws

--- src/main/java/at/fhv/backend/PlayerMoveMessage.java
+++ src/main/java/at/fhv/backend/PlayerMoveMessage.java
@@ -1,18 +1,19 @@
 package at.fhv.backend;
 
-import at.fhv.backend.model.Player;
+
 import at.fhv.backend.model.Position;
 
+
 public class PlayerMoveMessage {
-    private int playerID;
+    private int id;
     private Position newPosition;
 
-    public int getPlayerID() {
-        return playerID;
+    public int getId() {
+        return id;
     }
 
-    public void setPlayerID(int playerID) {
-        this.playerID = playerID;
+    public void setId(int id) {
+        this.id = id;
     }
 
     public Position getNewPosition() {
