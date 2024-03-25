fb7a5bbf5401bd8c895feda19d297f819a835c56
Sarah Neville <sarah.neville@students.fhv.at>
2024-03-26 00:48:43 +0100

    added Map class

---
+++ src/main/java/at/fhv/backend/model/Map.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2024 Sarah N
+ *
+ * Project Name:         AmongUs_Replica_Backend
+ * Description:
+ *
+ * Date of Creation/
+ * Last Update:          26/03/2024
+ */
+
+package at.fhv.backend.model;
+
+public class Map {
+    private boolean[][] map;
+
+    public Map() {
+    }
+
+    public boolean[][] getMap() {
+        return map;
+    }
+
+    public void setMap(boolean[][] mapArray) {
+        this.map = mapArray;
+    }
+
+    public void setMapbyPosition(int x, int y, boolean value) {
+        map[x][y] = value;
+    }
+
+    public void setInitialMap() {
+        if (map == null) {
+            map = MapLoader.loadMapFromFile();
+        }
+    }
+
+}
