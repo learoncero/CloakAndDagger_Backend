52929deb35f8e48ec93e5016ba843783e8a275a5
Sarah Neville <sarah.neville@students.fhv.at>
2024-03-26 11:38:47 +0100

    added mapService to playerService, added validation to movement

--- src/main/java/at/fhv/backend/service/PlayerService.java
+++ src/main/java/at/fhv/backend/service/PlayerService.java
@@ -9,9 +9,11 @@ import java.util.List;
 @Service
 public class PlayerService {
     private final PlayerRepository playerRepository;
+    private final MapService mapService;
 
-    public PlayerService(PlayerRepository playerRepository) {
+    public PlayerService(PlayerRepository playerRepository, MapService mapservice) {
         this.playerRepository = playerRepository;
+        this.mapService = mapservice;
     }
 
     public Player createPlayer(int id, String username, int x, int y) {
@@ -27,12 +29,23 @@ public class PlayerService {
     public void updatePlayerPosition(int id, Position position) {
         System.out.println("updatePlayerPosition called in PlayerService");
         Player player = playerRepository.findById(id).orElse(null);
-        if (player != null) {
-            player.setPosition(position);
-            playerRepository.save(player);
-            List<Player> players = playerRepository.findAll();
-            for(Player p: players) {
-                System.out.println("Player ID: " + p.getId() + " Player Username " + p.getUsername() + "Position: " + p.getPosition().getX() + ", " + p.getPosition().getY());
+        int x = position.getX();
+        int y = position.getY();
+        boolean outOfBounds =
+                (x < 0) ||
+                (y < 0) ||
+                (y >= mapService.getMap().getMap().length) || //zeilen
+                (x >= mapService.getMap().getMap()[0].length); //spalten
+
+        if (player != null && mapService != null){
+            if (!outOfBounds && mapService.isCellWalkable(x, y)) { //if true update repo otherwise do nothing
+                player.setPosition(position);
+                playerRepository.save(player);
+                /* //For debugging purposes
+                List<Player> players = playerRepository.findAll();
+                for(Player p: players) {
+                    System.out.println("Player ID: " + p.getId() + ", Username " + p.getUsername() + ", Position: " + p.getPosition().getX() + ", " + p.getPosition().getY());
+                }*/
             }
         }
     }
