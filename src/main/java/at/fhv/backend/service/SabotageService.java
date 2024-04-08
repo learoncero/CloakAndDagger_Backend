3a32ee07b350f8ba3fd5517aaedc168d12dbcc6e
Lea Roncero PC <lea.roncero@students.fhv.at>
2024-04-08 20:14:36 +0200

    added sabotages - model class, repo and service

---
+++ src/main/java/at/fhv/backend/service/SabotageService.java
@@ -0,0 +1,20 @@
+package at.fhv.backend.service;
+
+import at.fhv.backend.model.Sabotage;
+import at.fhv.backend.repository.SabotageRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SabotageService {
+    private final SabotageRepository sabotageRepository;
+
+    public SabotageService(SabotageRepository sabotageRepository) {
+        this.sabotageRepository = sabotageRepository;
+    }
+
+    public List<Sabotage> getAllSabotages() {
+        return sabotageRepository.getAllSabotages();
+    }
+}
