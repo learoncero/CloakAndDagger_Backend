3a028a177453280c2974f4b69e4a4a5ca62b97cc
Lea Roncero PC <lea.roncero@students.fhv.at>
2024-04-19 19:38:15 +0200

    adjusted GameServiceTest to new GameService class

--- src/test/java/at/fhv/backend/utils/GameCodeGeneratorTest.java
+++ src/test/java/at/fhv/backend/utils/GameCodeGeneratorTest.java
@@ -1,6 +1,5 @@
-package at.fhv.backend;
+package at.fhv.backend.utils;
 
-import at.fhv.backend.utils.GameCodeGenerator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
