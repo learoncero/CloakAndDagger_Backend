8a8d989badf5b79b05e7df6b34e628193ca77b01
Sarah Neville <sarah.neville@students.fhv.at>
2024-05-03 13:02:48 +0200

    sabotages are now being loaded correctly when game is created

--- Game/src/main/java/at/fhv/game/service/SabotageService.java
+++ Game/src/main/java/at/fhv/game/service/SabotageService.java
@@ -1,11 +1,11 @@
 package at.fhv.game.service;
 
 import at.fhv.game.model.Game;
+import at.fhv.game.model.Position;
 import at.fhv.game.model.Sabotage;
 import at.fhv.game.repository.SabotageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
@@ -21,8 +21,17 @@ public class SabotageService {
         return sabotageRepository.getAllSabotages();
     }
 
+    private List<Sabotage> resetSabotagePositions(List<Sabotage> sabotages){
+        for (Sabotage s : sabotages){
+            s.setPosition(new Position(-1, -1));
+        }
+        return sabotages;
+    }
+
     public void addSabotagesToGame(Game game) {
-        game.setSabotages(getAllSabotages());
+        List<Sabotage> sabotages;
+        sabotages = resetSabotagePositions(getAllSabotages());
+        game.setSabotages(sabotages);
         System.out.println("Sabotages after adding them to game in SabotageService: ");
         for (Sabotage s: game.getSabotages()){
             System.out.println("Sabotage id: " + s.getId() +
