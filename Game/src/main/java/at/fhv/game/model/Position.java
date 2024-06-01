844cec3eed0aa2d8cf93eb7b6a2c1dfdda0b1b92
Lea Roncero PC <lea.roncero@students.fhv.at>
2024-06-01 10:40:18 +0200

    implemented player collision for crewmates and impostors (not ghosts)

--- Game/src/main/java/at/fhv/game/model/Position.java
+++ Game/src/main/java/at/fhv/game/model/Position.java
@@ -12,4 +12,12 @@ import lombok.Setter;
 public class Position {
     private int x;
     private int y;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return x == position.x && y == position.y;
+    }
 }
