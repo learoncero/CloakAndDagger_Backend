6482cdb78274090ceb8dd9acdddd6a55774d1577
Sarah Neville <sarah.neville@students.fhv.at>
2024-06-03 08:37:08 +0200

    added function to get vent Positions when maps are initialised

--- Game/src/main/java/at/fhv/game/model/Map.java
+++ Game/src/main/java/at/fhv/game/model/Map.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -20,6 +22,9 @@ public class Map {
     @Schema(description = "The name of the map")
     private String name;
 
+    @Schema(description = "List of vent positions (in pairs) for the map")
+    private List<List<Position>> ventPositions;
+
     public Map() {
     }
 
@@ -27,9 +32,32 @@ public class Map {
         this.id = nextId++;
         this.map = map;
         this.name = name;
+        this.ventPositions = getVentPositions(map);
+        System.out.println("ventPositions: ");
+        for(List<Position> ventPos : ventPositions) {
+            System.out.println(ventPos.get(0).getX() + ", " + ventPos.get(0).getY() +
+                    " - " + ventPos.get(1).getX() + ", " + ventPos.get(1).getY());
+        }
     }
 
     public char getCellValue(int x, int y) {
         return map[y][x];
+    }
+
+    public List<List<Position>> getVentPositions(char[][] map) {
+        List<List<Position>> ventPositions = new ArrayList<>();
+        for(int i = 0; i < map.length; i++) {
+            for(int j = 0; j < map[i].length; j++) {
+                if(Character.isDigit(map[i][j])) {
+                    Position ventPos = new Position(j, i);
+                    int charValue = Character.getNumericValue(map[i][j]);
+                    while (ventPositions.size() <= charValue) {
+                        ventPositions.add(new ArrayList<>());
+                    }
+                    ventPositions.get(charValue).add(ventPos);
+                }
+            }
+        }
+        return ventPositions;
     }
 }
